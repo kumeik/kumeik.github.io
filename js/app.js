c555d158f729c23ec4fae9a09f20660c91d93c62
kumeik <stivenbaci@outlook.com>
2017-01-14 10:41:43 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,15 +2,14 @@ var kuMeIk = angular.module('kuMeIk', [] );
 
 kuMeIk.controller('kuMeIk', function($scope) {
 	$scope.kategorite = [ 
-		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)'}, 
-		{emri: 'Bare', pershkrimi: ''}, 
-		{emri: 'Snacks', pershkrimi: ''}, 
-		{emri: 'Lounge', pershkrimi: ''}, 
-		{emri: 'Pub', pershkrimi: ''}, 
-		{emri: 'Club', pershkrimi: ''}, 
-		{emri: 'Fast Food', pershkrimi: ''}, 
-		{emri: 'Piceri', pershkrimi: ''},
-		{emri: 'Emb&euml;lsira', pershkrimi: ''}
+		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)', foto: 'img/restorante.png'}, 
+		{emri: 'Bare', pershkrimi: '', foto: 'img/bare.png'}, 
+		{emri: 'Snacks', pershkrimi: '', foto: 'img/snacks.png'}, 
+		{emri: 'Lounge', pershkrimi: '', foto: 'img/lounge.png'}, 
+		{emri: 'Club', pershkrimi: '', foto: 'img/club.png'}, 
+		{emri: 'Fast Food', pershkrimi: '', foto: 'img/fastfood.png'}, 
+		{emri: 'Piceri', pershkrimi: '', foto: 'img/piceri.png'},
+		{emri: 'Embelsira', pershkrimi: '', foto: 'img/embelsira.png'}
 	];
 
 	$scope.restorante = [
@@ -22,3 +21,19 @@ kuMeIk.controller('kuMeIk', function($scope) {
 	];
 
 });
+
+
+
+
+
+//TO DO LIST:
+
+	// Footer
+	// Header/Footer angularJS component
+	// Gjithsej function
+	//
+	//
+	//
+	//
+	//
+	//

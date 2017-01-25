ec73bb45fa0340928d67244b93a34452a766ef0b
StiviBaci <stivenbaci@yahoo.com>
2017-01-25 20:19:45 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,38 +2,50 @@ var kuMeIk = angular.module('kuMeIk', [] );
 
 kuMeIk.controller('kuMeIk', function($scope) {
 	$scope.kategorite = [ 
-		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)', foto: 'img/restorante.png'}, 
-		{emri: 'Bare', pershkrimi: '', foto: 'img/bare.png'}, 
-		{emri: 'Snacks', pershkrimi: '', foto: 'img/snacks.png'}, 
-		{emri: 'Lounge', pershkrimi: '', foto: 'img/lounge.png'}, 
-		{emri: 'Club', pershkrimi: '', foto: 'img/club.png'}, 
-		{emri: 'Fast Food', pershkrimi: '', foto: 'img/fastfood.png'}, 
-		{emri: 'Piceri', pershkrimi: '', foto: 'img/piceri.png'},
-		{emri: 'Embelsira', pershkrimi: '', foto: 'img/embelsira.png'}
+		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)', foto: 'img/restorante.png', link: 'restorante.html'}, 
+		{emri: 'Bare', pershkrimi: '', foto: 'img/bare.png', link: 'restorante.html'}, 
+		{emri: 'Snacks', pershkrimi: '', foto: 'img/snacks.png', link: 'restorante.html'}, 
+		{emri: 'Lounge', pershkrimi: '', foto: 'img/lounge.png', link: 'restorante.html'}, 
+		{emri: 'Club', pershkrimi: '', foto: 'img/club.png', link: 'restorante.html'}, 
+		{emri: 'Fast Food', pershkrimi: '', foto: 'img/fastfood.png', link: 'restorante.html'}, 
+		{emri: 'Piceri', pershkrimi: '', foto: 'img/piceri.png', link: 'restorante.html'},
+		{emri: 'Embelsira', pershkrimi: '', foto: 'img/embelsira.png', link: 'restorante.html'},
+		{emri: 'Hotele', pershkrimi: '', foto: 'img/hotel.png', link: 'restorante.html'}
 	];
 
 	$scope.restorante = [
-		{rank: 1, emri: '', pershkrimi: '', foto: ''},
-		{rank: 2, emri: '', pershkrimi: '', foto: ''},
-		{rank: 3, emri: '', pershkrimi: '', foto: ''},
-		{rank: 4, emri: '', pershkrimi: '', foto: ''},
+		{rank: 1, emri: 'La Voglia', pershkrimi: '', foto: ''},
+		{rank: 2, emri: 'Artigiano', pershkrimi: '', foto: ''},
+		{rank: 3, emri: 'Vita99', pershkrimi: '', foto: ''},
+		{rank: 4, emri: '2 Oxhaket', pershkrimi: '', foto: ''},
 		{rank: 5, emri: '', pershkrimi: '', foto: ''}
 	];
 
 });
 
 
+	kuMeIk.directive('header', function(){
+		return {
+			template: "<h1> Ku me ik?</h1>"
+		};
 
+	});
+
+	kuMeIk.directive('footer', function() {
+		return {
+			template: "INFO"
+		};
+	});
 
 
 //TO DO LIST:
 
 	// Footer
-	// Header/Footer angularJS component
+	// Footer angularJS component
 	// Gjithsej function
-	//
-	//
-	//
+	// gjithsej position
+	// faqet e tjera
+	// linkime
 	//
 	//
 	//

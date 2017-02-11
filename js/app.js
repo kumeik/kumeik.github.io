99a7ef557836533f4051c678cb9eb0b2d13cdc28
StiviBaci <stivenbaci@yahoo.com>
2017-02-11 10:51:32 +0100

    Add files via upload
    >New images
    >Added proper links
    >CSS fix for ugly links

--- js/app.js
+++ js/app.js
@@ -2,38 +2,50 @@ var kuMeIk = angular.module('kuMeIk', [] );
 
 kuMeIk.controller('kuMeIk', function($scope) {
 	$scope.kategorite = [ 
-		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)', foto: 'img/restorante.png'}, 
-		{emri: 'Bare', pershkrimi: '', foto: 'img/bare.png'}, 
-		{emri: 'Snacks', pershkrimi: '', foto: 'img/snacks.png'}, 
-		{emri: 'Lounge', pershkrimi: '', foto: 'img/lounge.png'}, 
-		{emri: 'Club', pershkrimi: '', foto: 'img/club.png'}, 
-		{emri: 'Fast Food', pershkrimi: '', foto: 'img/fastfood.png'}, 
-		{emri: 'Piceri', pershkrimi: '', foto: 'img/piceri.png'},
-		{emri: 'Embelsira', pershkrimi: '', foto: 'img/embelsira.png'}
+		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)', foto: 'img/restorante.png', link: 'restorante.html'}, 
+		{emri: 'Bare', pershkrimi: '', foto: 'img/bare.png', link: 'bare.html'}, 
+		{emri: 'Snacks', pershkrimi: '', foto: 'img/snacks.png', link: 'snacks.html'}, 
+		{emri: 'Lounge', pershkrimi: '', foto: 'img/lounge.png', link: 'lounge.html'}, 
+		{emri: 'Club', pershkrimi: '', foto: 'img/club.png', link: 'club.html'}, 
+		{emri: 'Fast Food', pershkrimi: '', foto: 'img/fastfood.png', link: 'fastfood.html'}, 
+		{emri: 'Piceri', pershkrimi: '', foto: 'img/piceri.png', link: 'piceri.html'},
+		{emri: 'Embelsira', pershkrimi: '', foto: 'img/embelsira.png', link: 'embelsira.html'},
+		{emri: 'Hotele', pershkrimi: '', foto: 'img/hotel.png', link: 'hotele.html'}
 	];
 
 	$scope.restorante = [
-		{rank: 1, emri: '', pershkrimi: '', foto: ''},
-		{rank: 2, emri: '', pershkrimi: '', foto: ''},
-		{rank: 3, emri: '', pershkrimi: '', foto: ''},
-		{rank: 4, emri: '', pershkrimi: '', foto: ''},
+		{rank: 1, emri: 'La Voglia', pershkrimi: '', foto: ''},
+		{rank: 2, emri: 'Artigiano', pershkrimi: '', foto: ''},
+		{rank: 3, emri: 'Vita99', pershkrimi: '', foto: ''},
+		{rank: 4, emri: '2 Oxhaket', pershkrimi: '', foto: ''},
 		{rank: 5, emri: '', pershkrimi: '', foto: ''}
 	];
 
 });
 
 
+	kuMeIk.directive('header', function(){
+		return {
+			template: "<h1> Ku me ik?</h1>"
+		};
 
+	});
+
+	kuMeIk.directive('footer', function() {
+		return {
+			template: "<h6>INFO</h6>"
+		};
+	});
 
 
 //TO DO LIST:
 
-	// Footer
-	// Header/Footer angularJS component
+	// Footer - CSS
+	// Footer angularJS component
 	// Gjithsej function
-	//
-	//
-	//
+	// gjithsej position
+	// faqet e tjera
+	// linkime
 	//
 	//
 	//

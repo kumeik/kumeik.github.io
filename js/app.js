4038cbd57603cef67d60fb4d1b92dbda6e1d9ff9
StiviBaci <stivenbaci@yahoo.com>
2017-07-17 19:22:52 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,26 @@
 var kuMeIk = angular.module('kuMeIk', [] );
 
 kuMeIk.controller('kuMeIk', function($scope) {
-	$scope.kategorite = [ 
-		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)', foto: 'img/restorante.png', link: 'restorante.html'}, 
-		{emri: 'Bare', pershkrimi: '', foto: 'img/bare.png', link: 'bare.html'}, 
-		{emri: 'Snacks', pershkrimi: '', foto: 'img/snacks.png', link: 'snacks.html'}, 
-		{emri: 'Lounge', pershkrimi: '', foto: 'img/lounge.png', link: 'lounge.html'}, 
-		{emri: 'Club', pershkrimi: '', foto: 'img/club.png', link: 'club.html'}, 
-		{emri: 'Fast Food', pershkrimi: '', foto: 'img/fastfood.png', link: 'fastfood.html'}, 
+	$scope.kategorite = [
+		{emri: 'Restorante', pershkrimi: 'Restorante m plak, ktu haen gjona a kpt ;)', foto: 'img/restorante.png', link: 'restorante.html'},
+		{emri: 'Bare', pershkrimi: '', foto: 'img/bare.png', link: 'bare.html'},
+		{emri: 'Snacks', pershkrimi: '', foto: 'img/snacks.png', link: 'snacks.html'},
+		{emri: 'Lounge', pershkrimi: '', foto: 'img/lounge.png', link: 'lounge.html'},
+		{emri: 'Club', pershkrimi: '', foto: 'img/club.png', link: 'club.html'},
+		{emri: 'Fast Food', pershkrimi: '', foto: 'img/fastfood.png', link: 'fastfood.html'},
 		{emri: 'Piceri', pershkrimi: '', foto: 'img/piceri.png', link: 'piceri.html'},
 		{emri: 'Embelsira', pershkrimi: '', foto: 'img/embelsira.png', link: 'embelsira.html'},
 		{emri: 'Hotele', pershkrimi: '', foto: 'img/hotel.png', link: 'hotele.html'}
 	];
 
 	$scope.restorante = [
-		{rank: 1, emri: 'La Voglia', pershkrimi: '', foto: ''},
-		{rank: 2, emri: 'Artigiano', pershkrimi: '', foto: ''},
-		{rank: 3, emri: 'Vita99', pershkrimi: '', foto: ''},
-		{rank: 4, emri: '2 Oxhaket', pershkrimi: '', foto: ''},
+		{rank: 1, emri: 'La Voglia', pershkrimi: 'In the center of Tirana, with delicious dishes and spectacular atmosphere.', foto: 'img/restorante/lavoglia.png'},
+		{rank: 2, emri: 'Artigiano', pershkrimi: 'Abdyl Frasheri, Tirana, Albania 1000', foto: 'img/restorante/artigiano.jpg'},
+		{rank: 3, emri: 'Vita99', pershkrimi: 'Rruga Komuna e Parisit, pallatet 1Maj(perballe stadiumit Dinamo) , Tirana, Albania 1001', foto: 'img/restorante/vita99.jpg'},
+		{rank: 4, emri: '2 Oxhaket', pershkrimi: 'GREAT FOOD' foto: ''},
 		{rank: 5, emri: '', pershkrimi: '', foto: ''}
 	];
+
 
 });
 
@@ -31,21 +32,13 @@ kuMeIk.controller('kuMeIk', function($scope) {
 
 	});
 
-	kuMeIk.directive('footer', function() {
-		return {
-			template: "<h6>INFO</h6>"
-		};
-	});
-
 
 //TO DO LIST:
 
-	// Footer - CSS
 	// Footer angularJS component
-	// Gjithsej function
 	// gjithsej position
 	// faqet e tjera
-	// linkime
+	//
 	//
 	//
 	//
